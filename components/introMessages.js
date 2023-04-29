1abb54c3c882a934345bcf278dc32bf6b59e5ecb
Kai Kleinbard <57405817+cre8ture@users.noreply.github.com>
2023-04-29 13:12:21 -0400

    new time

--- components/introMessages.js
+++ components/introMessages.js
@@ -5,14 +5,14 @@ function getCurrentTime() {
   const amPm = hours < 12 ? 'AM' : 'PM';
   hours = hours % 12 || 12; // Convert to 12-hour format
   minutes = minutes < 10 ? '0' + minutes : minutes; // Add leading zero if minutes < 10
-  // return `${hours}:${minutes} ${amPm}`;
-  return `12:00pm`;
+  return `${hours}:${minutes} ${amPm}`;
+  // return `12:00pm`;
 
 }
 let timeNow = getCurrentTime();
 
 export const introMessages =
-  [{ author: "You", text: "Hello, Mathy. How can you help me?", time: "12:00pm", math: false},
+  [{ author: "You", text: "Hello, Mathy. How can you help me?", time: timeNow, math: false},
   { author: "Agent", text: "Input your algebra problems and I'll help you solve them :)", time: timeNow, math: false },
   { author: "You", text: "sqrt(60 + 4)", time: timeNow, math: false },
   { author: "Agent", text: "The answer is 8.", time: timeNow, math: false },
